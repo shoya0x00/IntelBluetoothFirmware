6301ba917d0a2491d657c0d2ee9ac1a7aab1e30e
shoya0x00 <shoya0x00@gmail.com>
2025-12-12 03:36:26 +0600

    Update Intel Bluetooth firmware support

--- IntelBTPatcher/IntelBTPatcher.hpp
+++ IntelBTPatcher/IntelBTPatcher.hpp
@@ -76,12 +76,14 @@ public:
     
     static IOReturn newHostDeviceRequest(void *that, IOService *provider, StandardUSB::DeviceRequest &request, void *data, IOMemoryDescriptor *descriptor, unsigned int &length,IOUSBHostCompletion *completion, unsigned int timeout);
     static IOReturn newAsyncIO(void *that, IOMemoryDescriptor* dataBuffer, uint32_t dataBufferLength, IOUSBHostCompletion* completion, uint32_t completionTimeoutMs);
+    static IOReturn newSyncIO(void *that, IOMemoryDescriptor* dataBuffer, uint32_t dataBufferLength, uint32_t& bytesTransferred, uint32_t completionTimeoutMs);
     static int newInitPipe(void *that, StandardUSB::EndpointDescriptor const *descriptor, StandardUSB::SuperSpeedEndpointCompanionDescriptor const *superDescriptor,AppleUSBHostController *controller, IOUSBHostDevice *device, IOUSBHostInterface *interface, unsigned char, unsigned short);
 
     
     mach_vm_address_t oldFindQueueRequest {};
     mach_vm_address_t oldHostDeviceRequest {};
     mach_vm_address_t oldAsyncIO {};
+    mach_vm_address_t oldSyncIO {};
     mach_vm_address_t oldInitPipe {};
     
 private:
